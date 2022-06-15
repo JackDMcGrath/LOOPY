a78ea9f4e0bb179b82592b95633080d8d4fcead2
JackDMcGrath <44168380+JackDMcGrath@users.noreply.github.com>
2022-06-15 20:55:42 +0100

    Update bashrc_LOOPY.sh
    Remove repeated line

--- bashrc_LOOPY.sh
+++ bashrc_LOOPY.sh
@@ -3,6 +3,5 @@ export LOOPY_PATH="$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)"
 export PATH="$LOOPY_PATH/bin:$PATH"
 export PATH="$LOOPY_PATH/LOOPY_bin:$PATH"
 export PYTHONPATH="$LOOPY_PATH/LiCSBAS_lib:$PYTHONPATH"
-export PATH="$LOOPY_PATH/LOOPY_bin:$PATH"
 export PYTHONPATH="$LOOPY_PATH/LOOPY_lib:$PYTHONPATH"
 
