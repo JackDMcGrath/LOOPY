f95c4686d5e1ca15e0790bda465016f1d18e5f6f
Jack McGrath <eejdm@leeds.ac.uk>
2022-07-11 09:57:58 +0100

    Comment out labels_tmp to reduce memory useage

--- LOOPY_bin/LOOPY01_find_errors.py
+++ LOOPY_bin/LOOPY01_find_errors.py
@@ -9,7 +9,7 @@ correct other IFGs
 
 New modules needed:
     - scipy
-    - pandas
+    - skimage
     - numba
 
 ===============
@@ -352,13 +352,15 @@ def mask_unw_errors(i):
     if i==v:
         print('        ({}/{}): renumbered {:.2f}'.format(i+1, n_ifg, time.time()-begin))
 
-    labels_tmp=np.zeros((length,width,len(vals)),dtype='float32')
+
+    #Labels_tmp only needed for numba regions, not number_regions
+    #labels_tmp=np.zeros((length,width,len(vals)),dtype='float32')
  #   if i==v:
 #        print('        ({}/{}): labels_tmp frame made with {} vals {:.2f}'.format(i+1, n_ifg, len(vals), time.time()-begin))
-    for ix, val in enumerate(vals):
+    #for ix, val in enumerate(vals):
  #       if i==v:
 #            print('        ({}/{}): Label_tmp val {} {:.2f}'.format(i+1, n_ifg, val, time.time()-begin))
-        labels_tmp[:,:,ix] = label((npi==val).astype('int'))[0]
+        #labels_tmp[:,:,ix] = label((npi==val).astype('int'))[0]
 
     if i==v:
         commence_num = time.time()
